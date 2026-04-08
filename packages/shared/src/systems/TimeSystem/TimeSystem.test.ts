0e592d8e52b34eac8e2538c69ec1b0344f925721
Weston Vincze <westonvincze@gmail.com>
2026-04-07 22:18:23 -0400

    ♻️ Refactor time system to be separate

--- packages/shared/src/systems/TimeSystem/TimeSystem.test.ts
+++ packages/shared/src/systems/TimeSystem/TimeSystem.test.ts
@@ -1,15 +1,14 @@
 import { createWorld } from "bitecs";
 import { describe, expect, it } from "vitest";
-import { createTimeSystem } from "./TimeSystem";
+import { updateWorldTime } from "./TimeSystem";
 
 describe("TimeSystem", () => {
   let world: World;
-  const timeSystem = createTimeSystem();
 
   it("updates the world's time correctly", () => {
     world = createWorld();
     world.time = { elapsed: 0, delta: 0, then: 0 };
-    timeSystem(world);
+    updateWorldTime(world);
     expect(world.time.elapsed).toBeGreaterThan(0);
     expect(world.time.delta).toBeGreaterThan(0);
     expect(world.time.then).toBeGreaterThan(0);
